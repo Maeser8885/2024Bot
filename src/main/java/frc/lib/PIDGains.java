8440ecc4dd09bfd815c80af9d351cd6121c3b9c9
Ishant <ishantpbatra1234@gmail.com>
2024-02-29 22:01:33 -0700

    nO MeRgE cOnFlIcTs

---
+++ src/main/java/frc/lib/PIDGains.java
@@ -0,0 +1,23 @@
+package frc.lib;
+
+import com.revrobotics.SparkPIDController;
+
+public class PIDGains {
+    public final double p;
+    public final double i;
+    public final double d;
+
+    public PIDGains(double _p, double _i, double _d) {
+        p = _p;
+        i = _i;
+        d = _d;
+    }
+
+    public static void setSparkMaxGains(SparkPIDController _controller, PIDGains _gains) {
+        _controller.setP(_gains.p);
+        _controller.setI(_gains.i);
+        _controller.setD(_gains.d);
+    }
+
+}
+
